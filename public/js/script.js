3e51fb4cb60da146d71bfbc16e973c94484b9056
Robs <robsonline@live.com>
2021-10-11 19:45:02 -0300

    config deploy

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const closeMessage = document.querySelector("#close")
 const message = document.querySelector("#message")
 
@@ -8,15 +7,4 @@ closeMessage.addEventListener("click", function (){
 
 setTimeout(() => {
     message.style.display = "none"
-=======
-const closeMessage = document.querySelector("#close")
-const message = document.querySelector("#message")
-
-closeMessage.addEventListener("click", function (){
-    message.style.display = "none"
-})
-
-setTimeout(() => {
-    message.style.display = "none"
->>>>>>> 9c121fe9826ad47db00a8ed910b10408e41596ff
 }, 5000)
